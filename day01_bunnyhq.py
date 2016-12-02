f661b89645c752ff0b02b86bf6e8ec530741764f
Joost Molenaar <j.j.molenaar@gmail.com>
2016-12-02 11:28:19 +0100

    Move input to text file

--- day01_bunnyhq.py
+++ day01_bunnyhq.py
@@ -39,36 +39,32 @@ def bunny_hq_twice(directions):
     #print visited
     return abs(x) + abs(y)
 
-PUZZLE = 'R3, L5, R1, R2, L5, R2, R3, L2, L5, R5, L4, L3, R5, L1, R3, R4, R1, L3, R3, L2, L5, L2, R4, R5, R5, L4, ' \
-       + 'L3, L3, R4, R4, R5, L5, L3, R2, R2, L3, L4, L5, R1, R3, L3, R2, L3, R5, L194, L2, L5, R2, R1, R1, L1, L5, ' \
-       + 'L4, R4, R2, R2, L4, L1, R2, R53, R3, L5, R72, R2, L5, R3, L4, R187, L4, L5, L2, R1, R3, R5, L4, L4, R2, R5, ' \
-       + 'L5, L4, L3, R5, L2, R1, R1, R4, L1, R2, L3, R5, L4, R2, L3, R1, L4, R4, L1, L2, R3, L1, L1, R4, R3, L4, R2, ' \
-       + 'R5, L2, L3, L3, L1, R3, R5, R2, R3, R1, R2, L1, L4, L5, L2, R4, R5, L2, R4, R4, L3, R2, R1, L4, R3, L3, L4, ' \
-       + 'L3, L1, R3, L2, R2, L4, L4, L5, R3, R5, R3, L2, R5, L2, L1, L5, L1, R2, R4, L5, R2, L4, L5, L4, L5, L2, L5, ' \
-       + 'L4, R5, R3, R2, R2, L3, R3, L2, L5'
-
-class Test01_BunnyHQ(unittest.TestCase):
-    def test_example1(self):
+class Test01A_BunnyHQ(unittest.TestCase):
+    def test_01a_example1(self):
         result = bunny_hq('R2, L3')
         self.assertEqual(result, 5)
 
-    def test_example2(self):
+    def test_01a_example2(self):
         result = bunny_hq('R2, R2, R2')
         self.assertEqual(result, 2)
 
-    def test_example3(self):
+    def test_01a_example3(self):
         result = bunny_hq('R5, L5, R5, R3')
         self.assertEqual(result, 12)
 
-    def test_answer(self):
-        result = bunny_hq(PUZZLE)
+    def test_01a_answer(self):
+        with open('input/day01.txt', 'r') as f:
+            puzzle = f.read()
+        result = bunny_hq(puzzle)
         self.assertEqual(result, 236)
         
-class Test01_BunnyHQ_2(unittest.TestCase):
-    def test_example1(self):
+class Test01B_BunnyHQ(unittest.TestCase):
+    def test_01a_example(self):
         result = bunny_hq_twice('R8, R4, R4, R8')
         self.assertEqual(result, 4)
 
-    def test_answer(self):
-        result = bunny_hq_twice(PUZZLE)
+    def test_01a_answer(self):
+        with open('input/day01.txt', 'r') as f:
+            puzzle = f.read()
+        result = bunny_hq_twice(puzzle)
         self.assertEqual(result, 182)
