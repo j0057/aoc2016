5a9817310f77da59db64c1c668a825f167cc68aa
Joost Molenaar <j.j.molenaar@gmail.com>
2016-12-02 14:58:53 +0100

    Code for day 2B

--- day02_bunnykeypad.py
+++ day02_bunnykeypad.py
@@ -10,13 +10,23 @@ keypad = [[_, _, _, _, _],
           [_, 7, 8, 9, _],
           [_, _, _, _, _]][::-1]
 
+keypad_b = [[_,  _,  _,  _,  _,  _,  _],
+            [_,  _,  _,  1,  _,  _,  _],
+            [_,  _,  2,  3,  4,  _,  _],
+            [_,  5,  6,  7,  8,  9,  _],
+            [_,  _, 10, 11, 12,  _,  _],
+            [_,  _,  _, 13,  _,  _,  _],
+            [_,  _,  _,  _,  _,  _,  _]][::-1]
+
 moves = {'U':(+1,0),
          'D':(-1,0),
          'L':(0,-1),
          'R':(0,+1)}
 
+chars = '0123456789ABCDEF'
+
 # turn iterator into list
-listify = lambda f: lambda *a, **k: list(f(*a, **k))
+stringify = lambda f: lambda *a, **k: ''.join(chars[n] for n in f(*a, **k))
 
 def listify(func):
     def listify_wrapped(*args, **kwargs):
@@ -31,9 +41,8 @@ def parse(directions):
             break
         yield line
 
-@listify
-def bunny_keypad(directions):
-    y, x = 2, 2
+@stringify
+def bunny_keypad(directions, keypad=keypad, y=2, x=2):
     for line in parse(directions):
         #print line
         for move in line:
@@ -44,13 +53,27 @@ def bunny_keypad(directions):
         #print '-->', keypad[y][x]
         yield keypad[y][x]
 
+def bunny_keypad_really(directions):
+    return bunny_keypad(directions, keypad_b, y=3, x=1)
+
 class Test02A_BunnyKeyPad(unittest.TestCase):
     def test_02a_example(self):
         result = bunny_keypad('ULL\nRRDDD\nLURDL\nUUUUD\n')
-        self.assertEqual(result, [1, 9, 8, 5])
+        self.assertEqual(result, '1985')
 
     def test_02a_answer(self):
         with open('input/day02.txt', 'r') as f:
             puzzle = f.read()
         result = bunny_keypad(puzzle)
-        self.assertEqual(result, [3, 3, 4, 4, 4])
+        self.assertEqual(result, '33444')
+
+class Test02B_BunnyKeyPad(unittest.TestCase):
+    def test_02b_example(self):
+        result = bunny_keypad_really('ULL\nRRDDD\nLURDL\nUUUUD\n')
+        self.assertEqual(result, '5DB3')
+
+    def test_02b_answer(self):
+        with open('input/day02.txt', 'r') as f:
+            puzzle = f.read()
+        result = bunny_keypad_really(puzzle)
+        self.assertEqual(result, '446A6')
