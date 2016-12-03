5d5b4315e625676ee9bb992194448cb3f594d09a
Joost Molenaar <j.j.molenaar@gmail.com>
2016-12-03 09:11:47 +0100

    Add solution to 3B

--- day03_triangles.py
+++ day03_triangles.py
@@ -2,10 +2,18 @@
 
 import unittest
 
+listify = lambda f: lambda *a, **k: list(f(*a, **k))
+
+@listify
 def parse(f):
     for line in f:
         if not line: continue
         yield [ int(item) for item in line.split() ]
+
+def transform_vert(lines):
+    for (line1, line2, line3) in zip(lines[::3], lines[1::3], lines[2::3]):
+        for x in range(3):
+            yield [ line1[x], line2[x], line3[x] ]
 
 def is_triangle(a, b, c):
     return a + b > c
@@ -28,3 +36,17 @@ class Test03A_Triangles(unittest.TestCase):
             triangles = parse(f)
             result = count_triangles(triangles)
         self.assertEqual(result, 1032)
+
+class Test03B_Triangles(unittest.TestCase):
+    def test_03b_example(self):
+        triangles = parse('101 301 501\n102 302 502\n103 303 503\n201 401 601\n202 402 602\n203 403 603\n'.split('\n'))
+        triangles = transform_vert(triangles)
+        result = count_triangles(triangles)
+        self.assertEqual(result, 6)
+
+    def test_03b_answer(self):
+        with open('input/day03.txt', 'r') as f:
+            triangles = parse(f)
+            triangles = transform_vert(triangles)
+            result = count_triangles(triangles)
+        self.assertEqual(result, 1838)
