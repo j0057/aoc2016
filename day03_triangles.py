71a51420df05d10f73dade082e5cf392350afba4
Joost Molenaar <j.j.molenaar@gmail.com>
2016-12-08 19:07:42 +0100

    Migrate tests for day 3

--- day03_triangles.py
+++ day03_triangles.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python2.7
 
-import unittest
-
 listify = lambda f: lambda *a, **k: list(f(*a, **k))
 
 @listify
@@ -25,28 +23,3 @@ def count_triangles(triangles):
             result += 1
     return result
 
-class Test03A_Triangles(unittest.TestCase):
-    def test_03a_example(self):
-        triangles = parse('5 10 25\n'.split('\n'))
-        result = count_triangles(triangles)
-        self.assertEqual(result, 0)
-
-    def test_03a_answer(self):
-        with open('input/day03.txt', 'r') as f:
-            triangles = parse(f)
-            result = count_triangles(triangles)
-        self.assertEqual(result, 1032)
-
-class Test03B_Triangles(unittest.TestCase):
-    def test_03b_example(self):
-        triangles = parse('101 301 501\n102 302 502\n103 303 503\n201 401 601\n202 402 602\n203 403 603\n'.split('\n'))
-        triangles = transform_vert(triangles)
-        result = count_triangles(triangles)
-        self.assertEqual(result, 6)
-
-    def test_03b_answer(self):
-        with open('input/day03.txt', 'r') as f:
-            triangles = parse(f)
-            triangles = transform_vert(triangles)
-            result = count_triangles(triangles)
-        self.assertEqual(result, 1838)
---
+++ tests/test_day03.py
@@ -0,0 +1,21 @@
+from day03_triangles import *
+
+def test_03a_example():
+    triangles = parse('5 10 25\n'.split('\n'))
+    assert count_triangles(triangles) == 0
+
+def test_03a_answer():
+    with open('input/day03.txt', 'r') as f:
+        triangles = parse(f)
+    assert count_triangles(triangles) == 1032
+
+def test_03b_example():
+    triangles = parse('101 301 501\n102 302 502\n103 303 503\n201 401 601\n202 402 602\n203 403 603\n'.split('\n'))
+    triangles = transform_vert(triangles)
+    assert count_triangles(triangles) == 6
+
+def test_03b_answer():
+    with open('input/day03.txt', 'r') as f:
+        triangles = parse(f)
+        triangles = transform_vert(triangles)
+    assert count_triangles(triangles) == 1838
