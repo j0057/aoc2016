ee854c0bd2ba18365cc72f4e1c3c1b68db510515
Joost Molenaar <j.j.molenaar@gmail.com>
2016-12-08 19:07:42 +0100

    Migrate tests for day 4

--- day04_checksums.py
+++ day04_checksums.py
@@ -1,6 +1,5 @@
 import collections
 import re
-import unittest
 
 wrap = lambda c: lambda f: lambda *a, **k: c(f(*a, **k))
 
@@ -34,34 +33,3 @@ def decode_room_names(rooms):
         if checksum(n) != c:
             continue
         yield (caesar(n, s), s)
-
-class Test04A_Checksums(unittest.TestCase):
-    def test_04a_example(self):
-        rooms = [
-            'aaaaa-bbb-z-y-x-123[abxyz]',
-            'a-b-c-d-e-f-g-h-987[abcde]',
-            'not-a-real-room-404[oarel]',
-            'totally-real-room-200[decoy]'
-        ]
-        result = sum_real_rooms(rooms)
-        self.assertEqual(result, 1514)
-
-    def test_04a_answer(self):
-        with open('input/day04.txt', 'r') as rooms:
-            result = sum_real_rooms(rooms)
-        self.assertEqual(result, 137896)
-
-class Test04B_ChecksumCipher(unittest.TestCase):
-    maxDiff = None
-
-    def test_04b_example(self):
-        result = caesar('qzmt-zixmtkozy-ivhz', 343)
-        self.assertEqual(result, 'very encrypted name')
-
-    def test_04b_answer(self):
-        with open('input/day04.txt', 'r') as rooms:
-            result = decode_room_names(rooms)
-        #print result
-        result = next(s for (n, s) in result
-                      if n == 'northpole object storage')
-        self.assertEqual(result, 501)
---
+++ tests/test_day04.py
@@ -0,0 +1,24 @@
+from day04_checksums import *
+
+def test_04a_example():
+    rooms = [
+        'aaaaa-bbb-z-y-x-123[abxyz]',
+        'a-b-c-d-e-f-g-h-987[abcde]',
+        'not-a-real-room-404[oarel]',
+        'totally-real-room-200[decoy]'
+    ]
+    assert sum_real_rooms(rooms) == 1514
+
+def test_04a_answer():
+    with open('input/day04.txt', 'r') as rooms:
+        assert sum_real_rooms(rooms) == 137896
+
+def test_04b_example():
+    assert caesar('qzmt-zixmtkozy-ivhz', 343) == 'very encrypted name'
+
+def test_04b_answer():
+    with open('input/day04.txt', 'r') as rooms:
+        result =  decode_room_names(rooms)
+        #print result
+        result = next(s for (n, s) in result if n == 'northpole object storage')
+    assert result == 501
