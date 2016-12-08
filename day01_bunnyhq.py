3c4c63c64d87c9d3adcd19eaf3df944af04dc013
Joost Molenaar <j.j.molenaar@gmail.com>
2016-12-08 19:07:42 +0100

    Migrate tests for day 1

--- day01_bunnyhq.py
+++ day01_bunnyhq.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2.7
 
 from math import sin, cos, radians
-import unittest
 
 dsin = lambda d: int(sin(radians(d)))
 dcos = lambda d: int(cos(radians(d)))
@@ -38,33 +37,3 @@ def bunny_hq_twice(directions):
     #print x, y
     #print visited
     return abs(x) + abs(y)
-
-class Test01A_BunnyHQ(unittest.TestCase):
-    def test_01a_example1(self):
-        result = bunny_hq('R2, L3')
-        self.assertEqual(result, 5)
-
-    def test_01a_example2(self):
-        result = bunny_hq('R2, R2, R2')
-        self.assertEqual(result, 2)
-
-    def test_01a_example3(self):
-        result = bunny_hq('R5, L5, R5, R3')
-        self.assertEqual(result, 12)
-
-    def test_01a_answer(self):
-        with open('input/day01.txt', 'r') as f:
-            puzzle = f.read()
-        result = bunny_hq(puzzle)
-        self.assertEqual(result, 236)
-        
-class Test01B_BunnyHQ(unittest.TestCase):
-    def test_01a_example(self):
-        result = bunny_hq_twice('R8, R4, R4, R8')
-        self.assertEqual(result, 4)
-
-    def test_01a_answer(self):
-        with open('input/day01.txt', 'r') as f:
-            puzzle = f.read()
-        result = bunny_hq_twice(puzzle)
-        self.assertEqual(result, 182)
---
+++ tests/test_day01.py
@@ -0,0 +1,23 @@
+from day01_bunnyhq import *
+
+def test_01a_example1():
+    assert bunny_hq('R2, L3') == 5
+
+def test_01a_example2():
+    assert bunny_hq('R2, R2, R2') == 2
+
+def test_01a_example3():
+    assert bunny_hq('R5, L5, R5, R3') == 12
+
+def test_01a_answer():
+    with open('input/day01.txt', 'r') as f:
+        puzzle = f.read()
+    assert bunny_hq(puzzle) == 236
+    
+def test_01b_example():
+    assert bunny_hq_twice('R8, R4, R4, R8') == 4
+
+def test_01b_answer():
+    with open('input/day01.txt', 'r') as f:
+        puzzle = f.read()
+    assert bunny_hq_twice(puzzle) == 182
